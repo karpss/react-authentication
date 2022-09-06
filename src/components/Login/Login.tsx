9ab61609b5377f2897db128bed70948515a3df9b
karpss <ola.fam10@gmail.com>
2022-09-06 18:07:31 +0100

    Developed Login form and added validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,45 @@
 /* eslint-disable */
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 
 function Login() {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [isInputValid, setIsInputValid] = useState(false);
+
+    const validateInput = useCallback( () => {
+        let validInput = true;
+        if (username === "" || password === ""){
+            validInput = false;
+        }
+        return validInput;
+    }, [username, password]);
+
+
+    const handleInputChange = (e: any) => {
+        setIsInputValid(validateInput());
+    }
+
+    useEffect(() => {
+        setIsInputValid(validateInput());
+      }, [username, password, validateInput]);
+      console.log(username);
+      console.log(password);
     return (
-      <div>
-        Login
+      <div className='loginForm'>
+        <form>
+        <input
+        type="text"
+        placeholder="Username"
+        onChange={(event) => handleInputChange(setUsername(event?.target.value))}
+        />
+        <input
+        type="password"
+        placeholder="Password"
+        onChange={(event) => handleInputChange(setPassword(event?.target.value))}
+        
+        />
+        <button type="submit" disabled={!isInputValid}>Login</button>
+        </form>
       </div>
     );
   }
